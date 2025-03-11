c1aca85994af1367d32473898601ed8a68401bc9
dennissammy77 <dennissammy77@gmail.com>
2025-03-11 12:51:39 +0300

    Handling array with find()

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(records){
+    const result = records.find((record)=>
+        record.result === "W"
+    )
+    if(!result){
+        return undefined
+    }
+    return result.year
+}
